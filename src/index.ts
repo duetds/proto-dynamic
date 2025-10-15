2efeb26268afd3c3bc50a3ba5885379bae03475b
Leo Jäppinen <leojappinen@Mac.station>
2025-10-15 12:56:45 +0200

    dynamic hero relocation

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { ProtoDynamicHero } from "./hero/proto-dynamic-hero"
+import { ProtoDynamicHero } from "./proto-dynamic-hero"
 import { MyElement } from "./my-element"
 import { ProtoDynamicTest } from "./proto-dynamic-test"
 
