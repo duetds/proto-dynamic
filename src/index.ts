6e8217a0593310a220c1cc4e37c3e2b992414b8f
Leo Jäppinen <leojappinen@Mac.station>
2025-10-15 12:57:35 +0200

    lint

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import { ProtoDynamicHero } from "./proto-dynamic-hero"
 import { MyElement } from "./my-element"
+import { ProtoDynamicHero } from "./proto-dynamic-hero"
 import { ProtoDynamicTest } from "./proto-dynamic-test"
 
 export { MyElement, ProtoDynamicTest, ProtoDynamicHero }
--- src/proto-dynamic-hero.ts
+++ src/proto-dynamic-hero.ts
@@ -92,7 +92,7 @@ export class ProtoDynamicHero extends LitElement {
   `
 
   override render() {
-    console.log('Dynamic Hero Properties', this.props)
+    console.log("Dynamic Hero Properties", this.props)
     const fields = this.props?.[0]?.fields
     const headingObject = fields?.heading // { value?: string } | undefined
     const introObject = fields?.intro
