00d8d0137de521b8bd3c07041ece00ed2584072b
Leo Jäppinen <leojappinen@Mac.station>
2025-11-05 14:48:35 +0100

    revert console log

--- src/utils/helper-functions.ts
+++ src/utils/helper-functions.ts
@@ -1,6 +1,3 @@
 export function isUrlExternal(url: string | undefined): boolean {
-  const test = !!url?.includes("https://")
-
-  console.log("TEST: ", test)
-  return test
+  return !!url?.includes("https://")
 }
