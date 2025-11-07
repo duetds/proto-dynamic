ff4386d9531dc8c068e219d0e7864469376901bf
Kasimir Pihlasviita <kasimir@pihlasviita.fi>
2025-11-07 17:16:36 +0100

    Remove PoC and sample components

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
 import { ProtoDynamicHero } from "./hero/proto-dynamic-hero"
 import { ProtoDynamicHighlight } from "./highlight/proto-dynamic-highlight"
 import { ProtoDynamicModule } from "./module/proto-dynamic-module"
-import { MyElement } from "./my-element"
-import { ProtoDynamicTest } from "./proto-dynamic-test"
 
-export { MyElement, ProtoDynamicTest, ProtoDynamicHero, ProtoDynamicHighlight, ProtoDynamicModule }
+export { ProtoDynamicHero, ProtoDynamicHighlight, ProtoDynamicModule }
