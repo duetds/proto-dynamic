ec65045eccc2aaea17b9a788749e2ad9df5fcfb1
Leo Jäppinen <leo.jappinen@dka.io>
2026-02-26 17:04:25 +0200

    props possible in RT component

--- src/rich-text/proto-dynamic-rich-text.ts
+++ src/rich-text/proto-dynamic-rich-text.ts
@@ -4,17 +4,25 @@ import { unsafeHTML } from "lit/directives/unsafe-html.js"
 import type { RichTextNode } from "../hero/proto-dynamic-hero"
 import { formatRichText, renderRichText } from "../utils/helper-functions"
 
+export interface ProtoDynamicRichTextProps {
+  data: RichTextNode[]
+  options?: {
+    margin?: "none" | "small" | "medium" | "large"
+    stylePreset?: "small" | "intro"
+    [key: string]: unknown
+  }
+}
+
 @customElement("proto-dynamic-rich-text")
 export class ProtoDynamicRichText extends LitElement {
-  @property({ type: Array }) props?: RichTextNode[]
+  @property({ attribute: false }) props: ProtoDynamicRichTextProps = { data: [], options: {} }
   @property({ type: String }) currentModalEntryId?: string
 
   override render() {
-    const fields = this.props
-    if (!fields) return
-    // TODO: pass RT props from inside the target project (margin, stylePreset, etc)
+    const { data, options } = this.props
+
     return html`
-      ${unsafeHTML(formatRichText(renderRichText(fields), { margin: "none" }))}
+      ${unsafeHTML(formatRichText(renderRichText(data), options))}
     `
   }
 }
